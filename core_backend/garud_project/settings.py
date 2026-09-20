468193bd95f0da65e68ba8190f9f5a26f44f5ce4
S.P. Vishwakarma <itspratap255@gmail.com>
2026-09-20 14:01:28 +0530

    Bugs Fix

--- core_backend/garud_project/settings.py
+++ core_backend/garud_project/settings.py
@@ -14,8 +14,13 @@ SECRET_KEY = 'django-insecure-puv&apyb$*oyi(p!bum5f6z_)!3cg$)*x1o$=15zbdmskq6u$@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['elegant-wholeness-production-cb9e.up.railway.app', 'localhost', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://elegant-wholeness-production-cb9e.up.railway.app']
+ALLOWED_HOSTS = ['*']
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://elegant-wholeness-production-cb9e.up.railway.app',
+    'https://*.up.railway.app'
+]
+
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
