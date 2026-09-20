9e3e192ee78e34d5b314f50b2270ef6821441c58
S.P. Vishwakarma <itspratap255@gmail.com>
2026-09-20 12:33:01 +0530

    Updated AI API URL

--- core_backend/reports/views.py
+++ core_backend/reports/views.py
@@ -130,8 +130,7 @@ def submit_incident(request):
             user_email = citizen.email
             user_phone = citizen.profile.phone if hasattr(citizen, 'profile') else "N/A"
             
-            fastapi_url = "http://127.0.0.1:8001/process-incident"
-            res = requests.post(fastapi_url, json=body)
+            fastapi_url = "[https://garudai-production.up.railway.app/process-incident](https://garudai-production.up.railway.app/process-incident)"            res = requests.post(fastapi_url, json=body)
             
             if res.status_code == 200:
                 api_response = res.json()
